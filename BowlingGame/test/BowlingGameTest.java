59dc909f3694eb72af5df4795509f182b1e17162
James_han <james.han@codingrobotlab.com>
2020-05-06 16:35:56 +0900

    first commit update

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -6,9 +6,8 @@ class BowlingGameTest {
 	@Test
 	public void testGutterGame() {
 		Game g = new Game();
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 20; i++)
 			g.roll(0);
-		}
 		assertEquals(0, g.score());
 	}
 
